9c6e6484c56c523024d07cfff09bb749b9cab6ff
Artem Kremlyov <gfvhhhggffghh@gmail.com>
2019-09-06 23:03:53 +0700

    slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
 let aside = document.querySelector('.aside-menu');
 let a = document.querySelectorAll('.aside-menu__link');
-for (let i = 0; i < a.length; i++) { a[i].addEventListener('click', function() { aside.style.right = '-100%' }); }
+for (let i = 0; i < a.length; i++) { a[i].addEventListener('click', function() { aside.style.right = '-100%';body.style.overflow = 'visible'; }); }
 
 let button = document.querySelector('.fullscr-btn');
 button.addEventListener ('click', function(){
@@ -12,6 +13,50 @@ cross.addEventListener('click', function(){
     aside.style.right = '10000px';
     document.body.style.overflow = 'visible';
 });
+
+
+const buttonRight = document.querySelector('.arrow-scroll--right');
+const buttonLeft = document.querySelector('.arrow-scroll--left');
+const sliderList = document.querySelector('.slider-list');
+
+
+
+buttonRight.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log('Right');
+    loop('buttonRight');
+})
+buttonLeft.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log('Left');
+    loop('buttonLeft');
+})
+
+
+function loop (directionButton){
+    if (directionButton === 'buttonRight'){
+       sliderList.appendChild(sliderList.firstChild);
+    }
+    else{
+        sliderList.insertBefore(sliderList.lastElementChild, sliderList.firstElementChild);
+    }
+}
+
+
+
+const sliderComposition = document.querySelector('.slider-composition');
+const sliderCross = document.querySelector('.slider-cross');
+const sliderDrop = document.querySelector('.slider-drop'); 
+
+sliderComposition.addEventListener('click', function(){
+    sliderDrop.style.left = '100%';
+    sliderDrop.style.display = 'block';
+})
+sliderCross.addEventListener('click', function(){
+    sliderDrop.style.display = 'none';
+    console.log('CLICK CLICK MOTHERFUCKER');
+})
+
 
 
 const accordeonTeam = document.querySelectorAll('.accordeon__item');
@@ -32,6 +77,25 @@ const accordeonTeamActive = document.querySelectorAll('accordoen__item--active')
  }
 
 
+    // Функция ymaps.ready() будет вызвана, когда
+   // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+   ymaps.ready(init);
+   function init(){ 
+       // Создание карты.    
+       var myMap = new ymaps.Map("map", {
+           // Координаты центра карты.
+           // Порядок по умолчанию: «широта, долгота».
+           // Чтобы не определять координаты центра карты вручную,
+           // воспользуйтесь инструментом Определение координат.
+           center: [55.0415, 82.9346],
+           // Уровень масштабирования. Допустимые значения:
+           // от 0 (весь мир) до 19.
+           zoom: 12
+       });
+       myMap.behaviors.disable('scrollZoom'); }
+});
+
+
  const accordeonMenu = document.querySelectorAll('.accordeon-menu__item');
 
   for (let k = 0;k<accordeonMenu.length;k++){
@@ -48,21 +112,21 @@ const accordeonTeamActive = document.querySelectorAll('accordoen__item--active')
 
   const body = document.body;
   const reviewsButton = document.querySelectorAll('.reviews__btn');
-  const headline = document.querySelector('.reviews__headline');
-  const text = document.querySelector('.reviews__text');
   const reviewsWindow = document.createElement('div');
   const reviewsBlock = document.createElement('div');
-  const reviewsText = document.createElement('div');
-  const reviewsHeadline = document.createElement('div');
   const reviewsCrossBlock = document.createElement('button');
   const reviewsCross = document.createElement('img');
+  let headline = document.querySelector('.reviews__headline');
+  let text = document.querySelector('.reviews__text');
+  let reviewsText = document.createElement('div');
+  let reviewsHeadline = document.createElement('div');
 for (n=0;n<reviewsButton.length;n++){
   reviewsButton[n].addEventListener('click', function(){
     reviewsWindow.style.display= 'flex';
     event.preventDefault();
     console.log('click');
     document.body.style.overflow='hidden';
-  
+
 body.appendChild(reviewsWindow);
 
 
@@ -113,29 +177,16 @@ reviewsHeadline.style.fontStyle = 'normal';
 reviewsHeadline.style.fontSize = '1.5rem';
   }) }
 
-if (reviewsCrossBlock.addEventListener('click',function(){
+reviewsCrossBlock.addEventListener('click',function(){
     reviewsWindow.remove(reviewsWindow);
     document.body.style.overflow = 'visible'
-}))
 
 
-document.addEventListener ('DOMContentLoaded',function(){
-    // Функция ymaps.ready() будет вызвана, когда
-   // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-   ymaps.ready(init);
-   function init(){ 
-       // Создание карты.    
-       var myMap = new ymaps.Map("map", {
-           // Координаты центра карты.
-           // Порядок по умолчанию: «широта, долгота».
-           // Чтобы не определять координаты центра карты вручную,
-           // воспользуйтесь инструментом Определение координат.
-           center: [55.0415, 82.9346],
-           // Уровень масштабирования. Допустимые значения:
-           // от 0 (весь мир) до 19.
-           zoom: 12
-       });
-       myMap.behaviors.disable('scrollZoom'); }
-});
+})
+
+
+
+
+
 
 
